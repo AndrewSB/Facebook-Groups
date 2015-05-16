7d88d426769ea605d0c3f069fe97d0a27d5c740c
Andrew Breckenridge <asbreckenridge@me.com>
2015-05-16 16:36:40 -0500

    add group vcs + group coreData type

---
+++ Facebook Groups/Group.swift
@@ -0,0 +1,16 @@
+//
+//  Group.swift
+//  
+//
+//  Created by Andrew Breckenridge on 5/16/15.
+//
+//
+
+import Foundation
+import CoreData
+
+class Group: NSManagedObject {
+
+    @NSManaged var name: String
+
+}
---
+++ Facebook Groups/GroupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GroupViewController.swift
+//  Facebook Groups
+//
+//  Created by Andrew Breckenridge on 5/16/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
