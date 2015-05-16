af31ab9944e6137f7a8b429b5004fd61913fcccd
Andrew Breckenridge <asbreckenridge@me.com>
2015-05-16 17:33:39 -0500

    add tableVC code to other 2 tables

--- Facebook Groups/PostViewController.swift
+++ Facebook Groups/PostViewController.swift
@@ -1,5 +1,5 @@
 //
-//  GroupViewController.swift
+//  PostViewController.swift
 //  Facebook Groups
 //
 //  Created by Andrew Breckenridge on 5/16/15.
@@ -8,46 +8,34 @@
 
 import UIKit
 
-class GroupViewController: UIViewController {
+class PostViewController: UIViewController {
     @IBOutlet weak var postTableView: UITableView!
-    
-    var posts: [Post]? {
+    var comments: [Comment]? {
         didSet {
             postTableView.reloadData()
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         postTableView.dataSource = self
         postTableView.delegate = self
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
-extension GroupViewController: UITableViewDataSource, UITableViewDelegate {
+extension PostViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts == nil ? 0 : posts!.count
+        return comments == nil ? 0 : comments!.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cellID") as! UITableViewCell
         
-        cell.textLabel?.text = posts![indexPath.row].description
+        cell.textLabel?.text = comments![indexPath.row].name
         cell.detailTextLabel?.text = "sdf"
         
         return cell
     }
 }
-
